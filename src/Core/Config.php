b78b6d9c987ee4f0eca859155a59271c763fcf8e
Rande A. Moreira <rande@adok.com.br>
2020-03-03 18:33:04 -0300

    Tratado certificado SSL em ambiente localhost

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -40,6 +40,7 @@ class Config
             ],
             'guzzle' => [
                 'base_url' => 'https://bling.com.br/Api/v2/',
+            	'defaults' => ['verify' => (defined('OASIS_DEBUG') && OASIS_DEBUG) ? false : true],
                 'timeout' => 30,
                 'headers' => [
                     'User-Agent' => 'BNW/1.0'
